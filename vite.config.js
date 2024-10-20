61c5e548fd0c647fa4cbe63bfbc63270569ebd6a
VishalPareek9024 <vishalpareek78.33@gmail.com>
2024-10-20 23:38:05 +0530

    update newnnnnnnn

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/TshirtDesign/', // Change this to your repository name
   plugins: [react()],
 });
-
